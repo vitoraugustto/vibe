a0fa017aecbe1f564c4e15d2ace3de83dcd21099
Vitor <vitoraugusto002@hotmail.com>
2025-09-01 14:01:39 -0300

    test(smoke): add should assertion to smoke test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,8 +1,9 @@
 /// <reference types="@cypress/grep" />
 
+
 describe('Smoke Test', { tags: '@smoke' }, () => {
   it('should load the home page', { tags: '@smoke' }, () => {
     cy.visit('/');
-    cy.contains('Next.js'); 
-  });
+    cy.contains('Next.js').should('exist');
+  }); 
 });

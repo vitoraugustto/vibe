ad70def0bb4fdcd9690ac9ec9bf74217e6d9a7cb
Vitor <vitoraugusto002@hotmail.com>
2025-09-01 14:02:40 -0300

    chore(cypress): update config with mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     supportFile: false,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
+    video: true,
+    screenshotOnRunFailure: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/mochawesome-report',
+      overwrite: false,
+      html: true,
+      json: true
+    },
     setupNodeEvents(on, config) {
       registerGrepPlugin(config);
       return config;
